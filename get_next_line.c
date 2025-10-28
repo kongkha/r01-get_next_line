87f6cf2d380d2593f12a2d5d0ad8db920e426235
Phakin Kongkha <pkongkha@student.42bangkok.com>
2025-10-29 04:14:35 +0700

    Mandatory

---
+++ get_next_line.c
@@ -0,0 +1,151 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pkongkha <pkongkha@student.42bangkok.com>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/22 19:49:22 by pkongkha          #+#    #+#             */
+/*   Updated: 2025/10/28 23:50:49 by pkongkha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+#include <stdlib.h>
+#include <stddef.h>
+#include <stdbool.h>
+#include <unistd.h>
+
+/**
+ * newline_len() - Length of string until newline
+ * @buf: pointer to buffer
+ * @len: maximum length to check
+ */
+static ssize_t	newline_len(char *buf, size_t len)
+{
+	const size_t	len_o = len;
+
+	while (len)
+	{
+		--len;
+		if (*buf == '\n')
+			return (len_o - len);
+		++buf;
+	}
+	return (-1);
+}
+
+/**
+ * read_fd_append_list() - Append and set lst to newly created node
+ * @lst: list
+ * @fd: file descriptor to read
+ * Return: new list
+ */
+static struct s_gnl_buf_list	*read_fd_append_list(
+	struct s_gnl_buf_list **lst, int fd)
+{
+	struct s_gnl_buf_list *const	new = gnl_lstnew();
+
+	if (!new)
+		return (NULL);
+	new->bytes_read = read(fd, new->buf, BUFFER_SIZE);
+	if (new->bytes_read <= 0)
+	{
+		free(new);
+		return (NULL);
+	}
+	if (*lst)
+		(*lst)->next = new;
+	*lst = new;
+	return (new);
+}
+
+/**
+ * read_fd_until_newline() - Read file descriptor until newline is found, and
+ *                           append into list
+ * @lst_info: list info, head must not be NULL
+ * @fd: file descriptor
+ * Return: length until newline
+ */
+static size_t	read_fd_until_newline(struct s_gnl_buf_list_info *lst_info,
+	int fd)
+{
+	struct s_gnl_buf_list	*lst;
+	size_t					bytes_curr;
+	ssize_t					len;
+
+	lst = lst_info->head;
+	bytes_curr = lst_info->bytes_curr;
+	while (true)
+	{
+		if (lst && lst->bytes_read)
+		{
+			len = newline_len(lst->buf + bytes_curr,
+					lst->bytes_read - bytes_curr);
+			if (len >= 0)
+			{
+				lst_info->bytes_eol += len;
+				return (lst_info->bytes_eol);
+			}
+			lst_info->bytes_eol += lst->bytes_read - bytes_curr;
+		}
+		if (!read_fd_append_list(&lst, fd))
+			return (0);
+		if (!lst_info->head)
+			lst_info->head = lst;
+		bytes_curr = 0;
+	}
+}
+
+/**
+ * construct_string() - Construct a string from provided lst_info,
+ *                      and delete completely traversed node
+ * @lst_info: list info
+ * @start: start of the list
+ * @n: number of character to construct
+ * Return: constructed string
+ */
+static char	*construct_string(struct s_gnl_buf_list_info *lst_info)
+{
+	ssize_t					to_write;
+	char *const				buf = malloc(lst_info->bytes_eol + 1);
+	ssize_t					written;
+
+	if (!buf)
+		return (NULL);
+	buf[lst_info->bytes_eol] = '\0';
+	written = 0;
+	while (lst_info->head && written != lst_info->bytes_eol)
+	{
+		to_write = min(lst_info->head->bytes_read - lst_info->bytes_curr,
+				lst_info->bytes_eol - written);
+		ft_memcpy(buf + written, lst_info->head->buf + lst_info->bytes_curr,
+			to_write);
+		written += to_write;
+		if (to_write == (lst_info->head->bytes_read - lst_info->bytes_curr))
+		{
+			gnl_delhead(&lst_info->head);
+			lst_info->bytes_curr = 0;
+		}
+		else
+			lst_info->bytes_curr += to_write;
+	}
+	lst_info->bytes_eol = 0;
+	return (buf);
+}
+
+/**
+ * get_next_line() - Get a line from file descriptor
+ * @fd: file descriptor to read
+ * Return: pointer to line string
+ */
+char	*get_next_line(int fd)
+{
+	static struct s_gnl_buf_list_info	lst_info = {
+		.head = NULL, .bytes_curr = 0, .bytes_eol = 0};
+
+	read_fd_until_newline(&lst_info, fd);
+	if (!lst_info.bytes_eol)
+		return (NULL);
+	return (construct_string(&lst_info));
+}
---
+++ get_next_line.h
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pkongkha <pkongkha@student.42bangkok.com>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/22 20:44:29 by pkongkha          #+#    #+#             */
+/*   Updated: 2025/10/28 23:36:12 by pkongkha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include <sys/types.h>
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 4096
+# endif
+
+struct	s_gnl_buf_list
+{
+	char					buf[BUFFER_SIZE];
+	ssize_t					bytes_read;
+	struct s_gnl_buf_list	*next;
+};
+
+struct	s_gnl_buf_list_info
+{
+	struct s_gnl_buf_list	*head;
+	ssize_t					bytes_curr;
+	ssize_t					bytes_eol;
+};
+
+char					*get_next_line(int fd);
+ssize_t					min(ssize_t a, ssize_t b);
+void					*ft_memcpy(void *dest, const void *src, size_t n);
+struct s_gnl_buf_list	*gnl_lstnew(void);
+struct s_gnl_buf_list	*gnl_lstadd_next(struct s_gnl_buf_list *lst,
+							struct s_gnl_buf_list *new);
+struct s_gnl_buf_list	*gnl_delhead(struct s_gnl_buf_list **head);
+#endif
